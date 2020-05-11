4fc073937ccaef84e85ded369fc33a66712e7f2e
dani337xx <dani337x@stud.kea.dk>
2020-05-11 13:51:02 +0200

    Prototype

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Clears the input field
+function eraseField(){
+    document.getElementById("noteField").value = "";
+}
+
+// Saves the input field into a new note
+function saveNote(){
+    
+}
+
+// Deletes the selected note
+function deleteNote(){
+    
+}
+
+// Opens the selected note
+function openNote(){
+    
+}
