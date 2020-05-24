6163ee7bc0236ee25d764a7cd119515988e8c869
dani337xx <>
2020-05-24 19:01:08 +0200

    moved and renamed the selenium folder

---
+++ Note-app_SeleniumWebDriver/confidential.cs
@@ -0,0 +1,11 @@
+﻿namespace NoteApp_SeleniumWebDriver
+{
+    static class confidential
+    {
+        private static string _localhost = "http://localhost:4200/";
+        private static string _driverLocation = "C:\\Users\\Daniel\\Desktop\\school\\TESTING\\Selenium assignment";
+
+        public static string Localhost { get => _localhost; }
+        public static string DriverLocation { get => _driverLocation; }
+    }
+}
