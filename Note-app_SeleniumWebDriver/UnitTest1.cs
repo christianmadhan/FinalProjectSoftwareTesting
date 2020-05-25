618122514b0646cec8607fe0322ffea6630bf26f
dani337xx <>
2020-05-25 22:05:46 +0200

    Selenium webdriver tests v1.0

--- Note-app_SeleniumWebDriver/UnitTest1.cs
+++ Note-app_SeleniumWebDriver/UnitTest1.cs
@@ -1,8 +1,10 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections;
+using System.Threading;
 
 namespace NoteApp_SeleniumWebDriver
 {
@@ -21,14 +23,255 @@ namespace NoteApp_SeleniumWebDriver
             driver.Url = weburl;
         }
 
-        /// <summary>
-        /// TEST METHODS
-        /// </summary>
+        // TITLE is empty
+        [Test]
+        public void title_is_empty()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            string expectedAlert = "Title can't be empty";
+
+            // ACT
+            titleField.SendKeys("");
+            noteField.SendKeys("Test note body");
+            saveButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // BODY is empty
+        [Test]
+        public void note_body_is_empty()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            string expectedAlert = "Note can't be empty";
+
+            // ACT
+            titleField.SendKeys("Test note title");
+            noteField.SendKeys("");
+            saveButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // Clear the content of the title field
+        [Test]
+        public void clear_title_field()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement eraseButton = driver.FindElement(By.Id("eraseButton"));
+
+            // ACT
+            titleField.SendKeys("Test note title");
+            noteField.SendKeys("Test note body");
+            eraseButton.Click();
+            Thread.Sleep(500);
+            string titleAfter = titleField.Text;
+
+            // ASSERT
+            Assert.AreEqual("", titleAfter);
+        }
+
+        // Clear the content of the body field
+        [Test]
+        public void clear_body_field()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement eraseButton = driver.FindElement(By.Id("eraseButton"));
+
+            // ACT
+            titleField.SendKeys("Test note title");
+            noteField.SendKeys("Test note body");
+            eraseButton.Click();
+            Thread.Sleep(500);
+            string titleAfter = noteField.Text;
+
+            // ASSERT
+            Assert.AreEqual("", titleAfter);
+        }
+
+        // TITLE is longer than 40 characters
+        [Test]
+        public void title_is_longer_than_40()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            string expectedAlert = "Title can max contain 40 characters";
+
+            // ACT
+            titleField.SendKeys("12345678901234567890123456789012345A-!@äx");
+            noteField.SendKeys("Test note body");
+            saveButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // BODY is longer than 500 characters
+        [Test]
+        public void body_is_longer_than_500()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            string expectedAlert = "note text can max contain 500 characters";
+            string noteBody = "";
+            for (int i = 0; i < 51; i++)
+            {
+                noteBody += "12345A-!@ä";
+            }
+
+            // ACT
+            titleField.SendKeys("Test note title");
+            noteField.SendKeys(noteBody);
+            saveButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // Trying to save another note, when we have 10/10 notes saved already
+        [Test]
+        public void trying_to_save_more_than_10_notes()
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            string expectedAlert = "You can only have 10 notes";
+
+            // ACT
+            titleField.SendKeys("Test note title");
+            noteField.SendKeys("Test note body");
+            for (int i = 0; i < 11; i++)
+            {
+                saveButton.Click();
+            }
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // Trying to open a note, without selecting one from the list
+        [Test]
+        public void trying_to_open_unselected_note()
+        {
+            // ARRANGE
+            close_Browser();
+            Setup();
+            Thread.Sleep(2000);
+            IWebElement openButton = driver.FindElement(By.Id("openButton"));
+            string expectedAlert = "You need to select a note";
+
+            // ACT
+            openButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
+
+        // Trying to delete a note, without selecting one from the list
+        [Test]
+        public void trying_to_delete_unselected_note()
+        {
+            // ARRANGE
+            close_Browser();
+            Setup();
+            Thread.Sleep(2000);
+            IWebElement deleteButton = driver.FindElement(By.Id("deleteButton"));
+            string expectedAlert = "You need to select a note";
+
+            // ACT
+            deleteButton.Click();
+            Thread.Sleep(1000);
+            string alertMessage = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            // ASSERT
+            Assert.AreEqual(alertMessage, expectedAlert);
+        }
 
         [TearDown]
         public void close_Browser()
         {
             driver.Close();
         }
+
+        // SAVE then OPEN notes with different sets of characters (using dataprovider to create multiple test cases)
+        [TestCase("Title One", "Body One")] // Latin
+        [TestCase("12345", "1234567890")] // numbers
+        [TestCase(@"~!@#$%^&*()-_=+[]\{}|;':,./<>?", @"~!@#$%^{}|;':,./<>?")] // special characters
+        [TestCase("ソフトウェアテスト", "ソフトウェアテスト ソフトウェアテスト")] // katakana
+        [TestCase("そふとうぇあてすと", "そふとうぇあてすと そふとうぇあてすと")] // hiragana
+        [TestCase("軟件測試", "軟件測試 軟件測試")] // kanji
+        [TestCase("тестирование программного обеспечения", "тестирование программного обеспечения тестирование")] // Cyrillic
+        [TestCase("소프트웨어 테스팅", "소프트웨어 테스팅 소프트웨어 테스팅")] // hangul
+        [TestCase("اختبار البرمجيات", "ااختبار البرمجيات ختبار البرمجيات")] // Arabic
+        [TestCase("सॉफ्टवेयर परीक्षण", "सॉफ्टवेयर परीक्षण सॉफ्टवेयर परीक्षण")] // Devanagari
+        [TestCase("בדיקת תוכנה", "בדיקת תוכנה בדיקת תוכנה")] // Hebrew
+        [TestCase("సాఫ్ట్‌వేర్ పరీక్ష", "సాఫ్ట్‌వేర్ పరీక్ష సాఫ్ట్‌వేర్ పరీక్ష")] // Telugu
+        [TestCase("ÅåÄäÖöÆæØø", "ÅåÄäÖöÆæØø ÅåÄäÖöÆæØø")] // Nordic special characters
+        public void save_then_open_notes_with_different_charsets(string originalTitle, string originalBody)
+        {
+            // ARRANGE
+            IWebElement titleField = driver.FindElement(By.Id("titleField"));
+            IWebElement noteField = driver.FindElement(By.Id("noteField"));
+            IWebElement saveButton = driver.FindElement(By.Id("saveButton"));
+            IWebElement openButton = driver.FindElement(By.Id("openButton"));
+            SelectElement noteList = new SelectElement(driver.FindElement(By.Id("savedNotes")));
+
+            // ACT
+            titleField.SendKeys(originalTitle);
+            noteField.SendKeys(originalBody);
+            saveButton.Click();
+            Thread.Sleep(500);
+
+            noteList.SelectByText(originalTitle);
+            openButton.Click();
+            Thread.Sleep(500);
+            string reopenedTitle = titleField.GetAttribute("value");
+            string reopenedBody = noteField.GetAttribute("value");
+
+            // ASSERT
+            Assert.AreEqual(reopenedTitle, originalTitle);
+            Assert.AreEqual(reopenedBody, originalBody);
+        }
+
+        // delete note
+        // sort notes
+        // saved notes counter
+        // add new note button when opening a saved note
     }
 }
