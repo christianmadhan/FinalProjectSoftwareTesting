fe964f7f5455366d8362ab4702caca2a770fc0a0
christianmadhan <christianmadhan@gmail.com>
2020-05-26 18:49:01 +0200

    created test.js

---
+++ test.js
@@ -0,0 +1,28 @@
+//--------------------------------------------------
+// Daniel
+// Save note test
+
+// Delete note test
+
+// Edit note test
+
+//--------------------------------------------------
+// Christian
+// Open note test
+
+// Erease note field test
+
+// Add new note test
+
+//--------------------------------------------------
+// Kolesar
+
+// Created saved notes list test
+
+// Sort by date note test
+
+//--------------------------------------------------
+// Ando
+// Sort by length note test
+
+// Sort by title note test
